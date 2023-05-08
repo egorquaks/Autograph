fb4eec50dcabf5ba8d5d4d2396973c6ffb16145e
qu4ks <egorlapatskij@gmail.com>
2023-05-09 00:50:11 +0300

    Creating plugin folder if that doesn't exist

--- src/main/java/by/quaks/autograph/Autograph.java
+++ src/main/java/by/quaks/autograph/Autograph.java
@@ -7,6 +7,13 @@ public final class Autograph extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+        if(!this.getDataFolder().exists()) { // Создание папки для хранения конфигурационных файлов
+            try {
+                this.getDataFolder().mkdir();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
         AphList.setup();
         getCommand("autograph").setExecutor(new AutographCMD());
     }
