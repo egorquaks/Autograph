e56f90702cf106c8a601faa38d1945155c5c2cd3
qu4ks <egorlapatskij@gmail.com>
2023-05-13 22:00:16 +0300

    Added getColor() method for color selection based on HEX codes or color names

--- src/main/java/by/quaks/autograph/Utils.java
+++ src/main/java/by/quaks/autograph/Utils.java
@@ -45,4 +45,47 @@ public class Utils {
         List<String> autographItemList = Config.get().getStringList("autographItemList");
         return autographItemList.contains(item.getType().toString());
     }
+    private static ChatColor getColor(String text) {
+        if (text.startsWith("#")) {
+            return ChatColor.of(text);
+        } else {
+            switch (text.toUpperCase()) {
+                case "BLACK":
+                    return ChatColor.BLACK;
+                case "DARK_BLUE":
+                    return ChatColor.DARK_BLUE;
+                case "DARK_GREEN":
+                    return ChatColor.DARK_GREEN;
+                case "DARK_AQUA":
+                    return ChatColor.DARK_AQUA;
+                case "DARK_RED":
+                    return ChatColor.DARK_RED;
+                case "DARK_PURPLE":
+                    return ChatColor.DARK_PURPLE;
+                case "GOLD":
+                    return ChatColor.GOLD;
+                case "GRAY":
+                    return ChatColor.GRAY;
+                case "DARK_GRAY":
+                    return ChatColor.DARK_GRAY;
+                case "BLUE":
+                    return ChatColor.BLUE;
+                case "GREEN":
+                    return ChatColor.GREEN;
+                case "AQUA":
+                    return ChatColor.AQUA;
+                case "RED":
+                    return ChatColor.RED;
+                case "LIGHT_PURPLE":
+                    return ChatColor.LIGHT_PURPLE;
+                case "YELLOW":
+                    return ChatColor.YELLOW;
+                case "WHITE":
+                    return ChatColor.WHITE;
+                default:
+                    return ChatColor.AQUA;
+            }
+        }
+    }
+
 }
