b9c3ecd9cedc4e66a7e39047ca3ec9d48e063031
qu4ks <egorlapatskij@gmail.com>
2023-11-18 02:26:34 +0300

    Commands & listeners registration

--- src/main/java/by/quaks/main/AutographPlugin.java
+++ src/main/java/by/quaks/main/AutographPlugin.java
@@ -1,9 +1,10 @@
 package by.quaks.main;
 
-import by.quaks.autograph.AnvilCraft;
 import by.quaks.main.commands.AutographCommand;
+import by.quaks.main.commands.RMAutographCommand;
 import by.quaks.main.config.ConfigsInitializer;
 import by.quaks.main.listeners.AnvilListener;
+import by.quaks.main.listeners.GrindstoneListener;
 import lombok.NonNull;
 import net.kyori.adventure.platform.bukkit.BukkitAudiences;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -23,7 +24,9 @@ public class AutographPlugin extends JavaPlugin {
         this.adventure = BukkitAudiences.create(this);
         new ConfigsInitializer();
         getCommand("autograph").setExecutor(new AutographCommand(adventure()));
+        getCommand("remove-autograph").setExecutor(new RMAutographCommand(adventure()));
         getServer().getPluginManager().registerEvents(new AnvilListener(this), this);
+        getServer().getPluginManager().registerEvents(new GrindstoneListener(), this);
     }
     @Override
     public void onDisable() {
