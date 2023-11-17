488f262f22a5eb682d529c70daacd824c7c643b5
qu4ks <egorlapatskij@gmail.com>
2023-11-18 02:24:04 +0300

    Refactoring&Updating&Fixing Utils.java

--- src/main/java/by/quaks/main/utils/Utils.java
+++ src/main/java/by/quaks/main/utils/Utils.java
@@ -1,10 +1,10 @@
 package by.quaks.main.utils;
 
-import by.quaks.autograph.Config;
 import by.quaks.main.config.MainConfig;
+import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
-import net.kyori.adventure.text.serializer.gson.GsonComponentSerializer;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
 import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Bukkit;
 import org.bukkit.inventory.ItemStack;
@@ -18,10 +18,14 @@ import java.util.regex.Pattern;
 public class Utils {
     public static boolean isAutographed(ItemStack itemStack){
         List<String> lore = getLore(itemStack);
-        String autographForm = MainConfig.get().getString("general.autograph");
+        String autographFormRaw = MainConfig.get().getString("general.autograph");
+        Component component = MiniMessage.miniMessage().deserialize(autographFormRaw);
+        String autographForm = PlainTextComponentSerializer.plainText().serialize(component);
         String autographPattern = autographForm.replaceAll("\\{player-name}","(.*?)");
         Pattern pattern = Pattern.compile(autographPattern);
-        return lore.stream().anyMatch(s -> pattern.matcher(ChatColor.stripColor(s)).find());
+        return lore.stream().anyMatch(s -> {
+            return pattern.matcher(ChatColor.stripColor(s)).find();
+        });
     }
     public static boolean hasAutographBy(ItemStack itemStack, String playerName){
         List<String> lore = getLore(itemStack);
@@ -34,7 +38,7 @@ public class Utils {
             return matcher.find();
         });
     }
-    public static void setLore(ItemStack item, String lore_text){
+    public static void addLore(ItemStack item, String lore_text){
         ItemMeta itemM = item.getItemMeta();
         List<String> lore = new ArrayList<>();
         assert itemM != null;
@@ -54,8 +58,8 @@ public class Utils {
         return LegacyComponentSerializer.builder().hexColors().useUnusualXRepeatedCharacterHexFormat().build().serialize(MiniMessage.miniMessage().deserialize(miniAutograph));
     }
     public static boolean canContainAutograph(ItemStack item){
-        List<String> autographItemList = Config.get().getStringList("autographItemList");
-        return autographItemList.contains(item.getType().toString());
+        List<String> autographItemList = MainConfig.get().getStringList("allowedItems");
+        return autographItemList.contains(item.getType().name());
     }
     public static String getItemName(ItemStack itemStack) {
         ItemMeta itemMeta = itemStack.getItemMeta();
@@ -67,10 +71,28 @@ public class Utils {
 
         return null;
     }
-    private static List<String> getLore(ItemStack itemStack){
-        ItemMeta itemMeta = itemStack.getItemMeta();
-        List<String> lore = new ArrayList<>();
-        if(itemMeta!=null) {lore = itemMeta.getLore();}
+    public static List<String> getLore(ItemStack itemStack) {
+        ItemMeta meta = itemStack.getItemMeta();
+        if (meta != null && meta.hasLore()) {
+            return meta.getLore();
+        }
+        return new ArrayList<>(); // Возвращаем пустой список, если Lore отсутствует
+    }
+    public static List<String> clearedLore(List<String> lore) {
+        String autographFormRaw = MainConfig.get().getString("general.autograph");
+        Component component = MiniMessage.miniMessage().deserialize(autographFormRaw);
+        String autographForm = PlainTextComponentSerializer.plainText().serialize(component);
+        String autographPattern = autographForm.replaceAll("\\{player-name}","(.*?)");
+        Pattern pattern = Pattern.compile(autographPattern);
+        return filterLore(lore,pattern);
+    }
+    public static List<String> filterLore(List<String> lore, Pattern pattern) {
+//        lore.forEach(s -> {
+//            Bukkit.getLogger().info(pattern.pattern());
+//            Bukkit.getLogger().info(ChatColor.stripColor(s));
+//        });
+        lore.removeIf(s -> pattern.matcher(ChatColor.stripColor(s)).find());
         return lore;
     }
+
 }
