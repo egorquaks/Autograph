f29681ba3105ab5441c8100ef8e358cf17f5b8d9
qu4ks <egorlapatskij@gmail.com>
2023-11-17 22:43:25 +0300

    Autograph by anvil final implementation

--- src/main/java/by/quaks/main/listeners/AnvilListener.java
+++ src/main/java/by/quaks/main/listeners/AnvilListener.java
@@ -15,6 +15,7 @@ import org.bukkit.inventory.ItemStack;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 import static org.bukkit.Bukkit.getServer;
 
@@ -36,7 +37,6 @@ public class AnvilListener implements Listener {
          */
         Player player = (Player) event.getView().getPlayer();
         AnvilInventory inventory = event.getInventory();
-        //Bukkit.getLogger().info(""+System.currentTimeMillis());
         if (previousInventories.put(player, inventory) == null) {
             onPrepareAnvil(event);
             getServer().getScheduler().runTask(plugin, task -> previousInventories.remove(player));
@@ -54,20 +54,32 @@ public class AnvilListener implements Listener {
             return;
         }
         ItemStack finalItem = baseItem.clone();
-        // Проверка на предмет для автографа ешё не сделана
         List<String> allowedItems = MainConfig.get().getStringList("allowedItems");
-        if(allowedItems.stream().anyMatch(baseItem.getType().toString()::equalsIgnoreCase)){
+        if(appliedItem.getType().name().equals(MainConfig.get().get("anvil.item.type"))){
+            if(MainConfig.get().getBoolean("anvil.item.useItemName")) {
+                Objects.requireNonNull(appliedItem.getItemMeta()).getDisplayName();
+                if(appliedItem.getItemMeta().getDisplayName().equals(MainConfig.get().getString("anvil.item.itemName"))){
+                    applyAutograph(event, baseItem, finalItem, allowedItems);
+                }
+            }else{
+                applyAutograph(event, baseItem, finalItem, allowedItems);
+            }
+        }
+    }
+
+    private void applyAutograph(PrepareAnvilEvent event, ItemStack baseItem, ItemStack finalItem, List<String> allowedItems) {
+        if (allowedItems.stream().anyMatch(baseItem.getType().toString()::equalsIgnoreCase)) {
             boolean hasAutographBy = Utils.hasAutographBy(baseItem, event.getView().getPlayer().getName());
             if (!hasAutographBy) {
                 Utils.setLore(finalItem, Utils.genAutograph(event.getView().getPlayer().getName()));
                 event.setResult(finalItem);
-                setRepairCost(event,10);
+                setRepairCost(event, MainConfig.get().getInt("anvil.autographCost"));
                 return;
             }
-            if(MainConfig.get().getBoolean("general.multipleAutographs")){
+            if (MainConfig.get().getBoolean("general.multipleAutographs")) {
                 Utils.setLore(finalItem, Utils.genAutograph(event.getView().getPlayer().getName()));
                 event.setResult(finalItem);
-                setRepairCost(event,10);
+                setRepairCost(event, MainConfig.get().getInt("anvil.autographCost"));
             }
         }
     }
