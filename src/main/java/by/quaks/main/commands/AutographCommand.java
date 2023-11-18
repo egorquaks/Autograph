3093049e1b46acba79d785fef1c364d47575cf91
qu4ks <egorlapatskij@gmail.com>
2023-11-18 16:10:30 +0300

    Autograph application fix

--- src/main/java/by/quaks/main/commands/AutographCommand.java
+++ src/main/java/by/quaks/main/commands/AutographCommand.java
@@ -5,7 +5,6 @@ import by.quaks.main.utils.Utils;
 import net.kyori.adventure.platform.bukkit.BukkitAudiences;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
-import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -13,26 +12,32 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class AutographCommand implements CommandExecutor {
     private final BukkitAudiences adventure;
-    public AutographCommand(BukkitAudiences adventure){
+
+    public AutographCommand(BukkitAudiences adventure) {
         this.adventure = adventure;
     }
+
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
-        if(commandSender instanceof Player player){
-            if(MainConfig.get().getBoolean("command.enabled")){
+        if (commandSender instanceof Player player) {
+            if (MainConfig.get().getBoolean("command.enabled")) {
                 ItemStack item = player.getInventory().getItemInMainHand();
                 if (item.getType().isAir()) {
-                    sendMessageToPlayer(player,"general.emptyHandMessage");
+                    sendMessageToPlayer(player, "general.emptyHandMessage");
                     return true;
                 }
-                List<String> allowedItems = MainConfig.get().getStringList("general.allowedItems");
-                allowedItems.forEach(String::toLowerCase);
-                if (allowedItems.contains(item.getType().name().toLowerCase())) {
-                    sendMessageToPlayer(player,"general.itemNotTheList");
+                List<String> allowedItems = MainConfig.get().getStringList("allowedItems");
+                List<String> allowedItemsLower = new ArrayList<>();
+                for (String material : allowedItems) {
+                    allowedItemsLower.add(material.toLowerCase());
+                }
+                if (!allowedItemsLower.contains(item.getType().name().toLowerCase())) {
+                    sendMessageToPlayer(player, "general.itemNotTheList");
                     return true;
                 } else {
                     boolean hasAutographBy = Utils.hasAutographBy(item, player.getName());
@@ -40,19 +45,21 @@ public class AutographCommand implements CommandExecutor {
                         Utils.addLore(item, Utils.genAutograph(player.getName()));
                         return true;
                     } else {
-                        sendMessageToPlayer(player,"general.itemContainsMaxAutographsBy");
-                    }
-                    if (MainConfig.get().getBoolean("general.multipleAutographs")) {
-                        Utils.addLore(item, Utils.genAutograph(player.getName()));
-                        return true;
+                        if (MainConfig.get().getBoolean("general.multipleAutographs")) {
+                            Utils.addLore(item, Utils.genAutograph(player.getName()));
+                            return true;
+                        } else {
+                            sendMessageToPlayer(player, "general.itemContainsMaxAutographsBy");
+                        }
                     }
                 }
-            }else{
-                sendMessageToPlayer(player,"command.disabledMessage");
+            } else {
+                sendMessageToPlayer(player, "command.disabledMessage");
             }
         }
         return true;
     }
+
     private void sendMessageToPlayer(Player player, String path) {
         Component message = MiniMessage.miniMessage().deserialize(MainConfig.get().getString(path));
         adventure.player(player).sendMessage(message);
